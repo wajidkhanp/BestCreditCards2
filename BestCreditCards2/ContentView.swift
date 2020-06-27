ac1ef76e6c38e41d659e2a473e2b73d7804fb4bb
wajid <wajid@live.ca>
2020-06-27 15:26:40 -0700

    Initial Commit

---
+++ BestCreditCards2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BestCreditCards2
+//
+//  Created by Wajid K Patan on 6/27/20.
+//  Copyright © 2020 wajidInc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
